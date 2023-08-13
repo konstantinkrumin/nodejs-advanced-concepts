74ea74df2a8a669337e6ab76c4b2a4f0915f436f
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-13 16:13:08 -0400

    added signin flow with puppeteer

--- blog-app/tests/header.test.js
+++ blog-app/tests/header.test.js
@@ -28,3 +28,29 @@ test('Clicking login starts OAuth flow', async () => {
 
 	expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test('When signed in, shows logout button', async () => {
+	const id = '64cd783353d1530f94ed5586';
+
+	const Buffer = require('safe-buffer').Buffer;
+	const sessionObject = {
+		passport: {
+			user: id
+		}
+	};
+	const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+	const Keygrip = require('keygrip');
+	const keys = require('../config/keys');
+	const keygrip = new Keygrip([keys.cookieKey]);
+	const sig = keygrip.sign('session=' + sessionString);
+
+	await page.setCookie({ name: 'session', value: sessionString });
+	await page.setCookie({ name: 'session.sig', value: sig });
+	await page.goto('localhost:3000');
+	await page.waitFor('a[href="/auth/logout"]');
+
+	const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+	expect(text).toEqual('Logout');
+});
