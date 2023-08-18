60a5c2a3d8b949ca15424b5f626ee3cdae90415c
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-18 14:36:31 -0400

    more configs for ci stuff

--- blog-app/config/ci.js
+++ blog-app/config/ci.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	mongoURI: process.env.DEV_MONGO_URI,
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
 	cookieKey: process.env.DEV_COOKIE_KEY,
 	redisUrl: 'redis://127.0.0.1:6379'
 };
