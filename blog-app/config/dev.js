309024e903de32ed84e188901a03c1709c57958e
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-24 15:09:40 -0400

    set up s3 upload logic

--- blog-app/config/dev.js
+++ blog-app/config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.DEV_MONGO_URI,
 	cookieKey: process.env.DEV_COOKIE_KEY,
-	redisUrl: 'redis://127.0.0.1:6379'
+	redisUrl: 'redis://127.0.0.1:6379',
+	accessKeyId: process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_SECRET_KEY
 };
